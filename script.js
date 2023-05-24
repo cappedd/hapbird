3f919d52dc0472972f1aeea7e652062805ba1b2f
cappedd <42228247+cappedd@users.noreply.github.com>
2023-05-25 00:29:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var css = document.querySelector("h3");
 var body = document.getElementById("gradient");
 var button = document.querySelector(".button");
 
@@ -5,6 +6,7 @@ function setGradient() {
 	var randomColor1 = Math.floor(Math.random()*16777215).toString(16);
 	var randomColor2 = Math.floor(Math.random()*16777215).toString(16);
 	body.style.background = "linear-gradient(to right, #" + randomColor1 + ", #" + randomColor2 + ")";
+	css.textContent = randomColor1 + " background change yes " + randomColor2
 }
 
 button.addEventListener("click", setGradient);
