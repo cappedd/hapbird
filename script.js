05c8603ec818e6389b2ce6ab5ca0bef6929b383a
cappedd <42228247+cappedd@users.noreply.github.com>
2023-05-25 00:14:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
@@ -5,6 +6,7 @@ var button = document.querySelector(".button");
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	css.textContent = body.style.background + ";"
 }
 
 function chooseRandom() {
@@ -15,4 +17,6 @@ function chooseRandom() {
 	setGradient();
 }
 
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
 button.addEventListener("click", chooseRandom);
